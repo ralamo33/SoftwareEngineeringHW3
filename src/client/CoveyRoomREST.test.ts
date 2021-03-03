d85859b971eb7127fe82aef4e86bf552e8a363ff
Ryan Alamo <ralamo@pop-os.localdomain>
2021-03-03 10:33:58 -0500

    Added in something

--- src/client/CoveyRoomREST.test.ts
+++ src/client/CoveyRoomREST.test.ts
@@ -203,11 +203,12 @@ describe('RoomServiceApiREST', () => {
       StartTest(testConfiguration);
       const roomInfoPublic = await apiClient.createRoom({ friendlyName: 'Enter Public', isPubliclyListed: true });
       const idPublic = roomInfoPublic.coveyRoomID; 
-      console.log(idPublic)
+      await apiClient.createRoom({ friendlyName:'This is not the room ID', isPubliclyListed: true })
       await apiClient.deleteRoom( { coveyRoomID: idPublic, coveyRoomPassword: roomInfoPublic.coveyRoomPassword });
       expect(apiClient.joinRoom( { userName: 'Masterchief', coveyRoomID: idPublic } )).rejects.toThrow();
       expect(apiClient.joinRoom( { userName: 'Ryan', coveyRoomID: 'NWmH2pUTIC-L' } )).rejects.toThrow();
       expect(apiClient.joinRoom( { userName: 'blair', coveyRoomID: '' } )).rejects.toThrow();
+      expect(apiClient.joinRoom( { userName: 'Masterchief', coveyRoomID: 'This is not the room ID' } )).rejects.toThrow();
     });
     it.each(ConfigureTest('MJPP'))('Admits a user to a valid public or private room [%s]', async (testConfiguration: string) => {
       StartTest(testConfiguration);
