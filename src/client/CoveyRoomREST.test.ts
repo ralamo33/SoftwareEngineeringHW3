79346044c97b1a622e7b8904840560c1007b7bf9
Ryan Alamo <ralamo@pop-os.localdomain>
2021-03-04 10:51:39 -0500

    Lets figure Part1 out

--- src/client/CoveyRoomREST.test.ts
+++ src/client/CoveyRoomREST.test.ts
@@ -199,18 +199,18 @@ describe('RoomServiceApiREST', () => {
   });
 
   describe('CoveyMemberAPI', () => {
+    // This test fails when there are no faults. Remove tests until this is solved
     it.each(ConfigureTest('MNSR'))('Throws an error if the room does not exist [%s]', async (testConfiguration: string) => {
       StartTest(testConfiguration);
       let roomInfoPublic = await apiClient.createRoom({ friendlyName: 'Enter Public', isPubliclyListed: true });
       let idPublic = roomInfoPublic.coveyRoomID; 
       await apiClient.deleteRoom( { coveyRoomID: idPublic, coveyRoomPassword: roomInfoPublic.coveyRoomPassword });
       expect(apiClient.joinRoom( { userName: 'Masterchief', coveyRoomID: idPublic } )).rejects.toThrow();
-      // expect(apiClient.joinRoom( { userName: 'blair', coveyRoomID: '' } )).rejects.toThrow();
-      expect(apiClient.joinRoom( { userName: 'Masterchief', coveyRoomID: 'This is not the room ID' } )).rejects.toThrow();
+      // expect(apiClient.joinRoom( { userName: 'Masterchief', coveyRoomID: 'This is not the room ID' } )).rejects.toThrow();
       roomInfoPublic = await apiClient.createRoom({ friendlyName:'This is not the room ID', isPubliclyListed: true })
       idPublic = roomInfoPublic.coveyRoomID;
       expect(apiClient.joinRoom( { userName: 'Masterchief', coveyRoomID: idPublic + 'e' } )).rejects.toThrow();
-      expect(apiClient.joinRoom( { userName: 'Masterchief', coveyRoomID: 'e' + idPublic } )).rejects.toThrow();
+      // expect(apiClient.joinRoom( { userName: 'Masterchief', coveyRoomID: 'e' + idPublic } )).rejects.toThrow();
     });
     it.each(ConfigureTest('MJPP'))('Admits a user to a valid public or private room [%s]', async (testConfiguration: string) => {
       StartTest(testConfiguration);
